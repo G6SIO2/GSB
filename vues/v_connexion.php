abb03a38513421570f1381504f03d4c6d9a4d37e
Admin <Admin@192.168.171.129>
2015-11-05 16:02:16 +0100

    - Corrections pour respecter les normes W3C

--- vues/v_connexion.php
+++ vues/v_connexion.php
@@ -5,11 +5,11 @@
 <form method="POST" action="index.php?uc=connexion&action=valideConnexion">
    
         <p>
-            <label for="nom">Login*</label>
+            <label for="login">Login*</label>
             <input id="login" type="text" name="login"  size="30" maxlength="45">
         </p>
         <p>
-            <label for="login">Mot de passe*</label>
+            <label for="mdp">Mot de passe*</label>
             <input id="mdp"  type="password"  name="mdp" size="30" maxlength="45">
         </p>
         <input type="submit" value="Valider" name="valider">
